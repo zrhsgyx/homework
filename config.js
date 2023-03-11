79e6ec9b217a4a5e474f347797a05909c23f9c60
zrhsgyx <104632178+zrhsgyx@users.noreply.github.com>
2023-03-11 17:56:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var _config = {
   // GitHub configuations.
   username: 'zrhsgyx',
   repo: 'https://github.com/zrhsgyx/homework.git',
-  access_token: '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',
+  access_token: '79CIoviuN1bRy7iJtCtkyv0D7SIKWq4HWEVV',
   
   // Validation.
   // If validation is set to true, only issues labelled with the
